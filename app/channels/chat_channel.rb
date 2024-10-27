bc56fb7b5bfa66a85706231967a211dad07dfb34
Aharon Seidman <aharondov1@gmail.com>
2024-10-27 12:51:14 -0400

    now sends name along with the messages

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -3,7 +3,16 @@ class ChatChannel < ApplicationCable::Channel
     chat  = Chat.find(params[:id])
     current_user.chats << chat
     stream_from "chat_channel#{params[:id]}"
-    transmit({messages: Chat.find(params[:id]).messages})
+    messages = Chat.find(params[:id]).messages.map do |message|
+      {
+        user_uuid: message.user_uuid,
+        kind: message.kind,
+        first_name: message.user.first_name,
+        last_name: message.user.last_name,
+        body: message.body
+      }
+    end
+    transmit({messages:})
   end
 
   def unsubscribed
@@ -23,6 +32,12 @@ class ChatChannel < ApplicationCable::Channel
 
   def speak(data)
     message = Chat.find(params[:id]).messages.create! user_id: current_user.id, body: data['body'],kind: data['kind'].to_sym, status: data['status'].to_sym, user_uuid: current_user.uuid
-    ActionCable.server.broadcast "chat_channel#{params[:id]}", {message:}
+    ActionCable.server.broadcast "chat_channel#{params[:id]}", {message: {
+      user_uuid: message.user_uuid,
+      kind: message.kind,
+      first_name: message.user.first_name,
+      last_name: message.user.last_name,
+      body: message.body
+    }}
   end
 end
