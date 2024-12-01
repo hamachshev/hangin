d134a58eae9e509f8d146e491a0619359dcd24b9
Aharon Seidman <aharondov1@gmail.com>
2024-12-01 11:50:28 -0500

    fixed apns error - needs the key before build so added to .env. deleted predeploy apns script

--- .platform/hooks/predeploy/getAPNSkey.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo aws s3api get-object --bucket $(/opt/elasticbeanstalk/bin/get-config environment -k S3_APPLICATION_BUCKET)  --key $(/opt/elasticbeanstalk/bin/get-config environment -k APNS_AUTHKEY) /var/app/current/$(/opt/elasticbeanstalk/bin/get-config environment -k APNS_AUTHKEY)
