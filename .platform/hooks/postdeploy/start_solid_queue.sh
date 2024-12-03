d5220bf709f8cdd28fc2bbb6e2fcb845e4b105e0
Aharon Seidman <aharondov1@gmail.com>
2024-12-02 22:47:58 -0500

    changed db config to delete extra tables and adjusted solid queue sh

--- .platform/hooks/postdeploy/start_solid_queue.sh
+++ .platform/hooks/postdeploy/start_solid_queue.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#cd /var/app/current
-#cd ../../../
-./bin/jobs &
+cd /var/app/current
+sudo touch ./solid_queue_log.log
+sudo chmod 666 ./solid_queue_log.log
+./bin/jobs > ./solid_queue_log.log 2>&1 & disown
 # Optionally, you can print the process ID for debugging or reference
 echo "Jobs script is running in the background with PID $!"
 exit 0
