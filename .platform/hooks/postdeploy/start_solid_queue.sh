dbf8a45d1ac30a317299c7544afa33d701033cfc
Aharon Seidman <aharondov1@gmail.com>
2024-12-02 21:20:11 -0500

    removed the cd in solid queue sh and addded echo and exit 0

--- .platform/hooks/postdeploy/start_solid_queue.sh
+++ .platform/hooks/postdeploy/start_solid_queue.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cd /var/app/current
-nohup ./bin/jobs &
+#cd /var/app/current
+#cd ../../../
+./bin/jobs &
+# Optionally, you can print the process ID for debugging or reference
+echo "Jobs script is running in the background with PID $!"
+exit 0
